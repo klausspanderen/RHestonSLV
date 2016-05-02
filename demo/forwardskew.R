995a6921f238062985c7898177c38a3e5ad76934
klausspanderen <klaus@spanderen.de>
2016-05-02 22:42:07 +0200

    copyright note updated

--- demo/forwardskew.R
+++ demo/forwardskew.R
@@ -1,4 +1,20 @@
-library(slv)
+#  Copyright (C) 2016 Klaus Spanderen
+#
+#  This file is part of RHestonSLV.
+#
+#  RHestonSLV is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 2 of the License
+#
+#  RHestonSLV is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with RHestonSLV.  If not, see <http:#www.gnu.org/licenses/>.
+
+library(RHestonSLV)
 library(parallel)
 
 vol <- 0.3
--- src/hestonslvmodule.cpp
+++ src/hestonslvmodule.cpp
@@ -1,3 +1,19 @@
+//  Copyright (C) 2016 Klaus Spanderen
+//
+//  This file is part of RHestonSLV.
+//
+//  RHestonSLV is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 2 of the License
+//
+//  RHestonSLV is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with RHestonSLV.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <ql/quotes/simplequote.hpp>
 #include <ql/time/calendars/nullcalendar.hpp>
 #include <ql/time/daycounters/actual365fixed.hpp>
